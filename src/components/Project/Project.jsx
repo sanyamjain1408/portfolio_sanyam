ccda351793596c05d8917f1b644ef2dcc8182d86
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-10 07:52:24 +0530

    web page of portfolio

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -13,7 +13,7 @@ function Project() {
   return (
     <div className="  flex flex-col pb-5  gap-5 justify-center items-center  sm:grid grid-cols-2 grid-rows-2 sm:gap-10 ">
      
-      <Link to="https://github.com/sanyamjain1408/Basic_Web_Page">
+      <Link to="https://sprightly-pastelito-e064fe.netlify.app/">
         <div className="flex flex-col justify-center items-center  ">
           <img
             className="w-[40rem] border-black mt-5   rounded-3xl p-3  shadow-2xl  hover:p-0 hover:shadow-black ease-in-out duration-200 "
